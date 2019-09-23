7e84a4e6c73c07379e36bfaa133b01e9ede4b115
juliaselezneva <juli.89@mail.ru>
2019-09-23 16:45:38 +0300

    dashboard component

--- trello/src/app/app.module.ts
+++ trello/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JunteUiModule } from 'junte-ui';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
